da115ef9559094fac0256427377cd988c3fcb57a
John Hamm <johndhamm1@gmail.com>
2016-06-16 16:02:58 -0500

    adding event listeners, end of work on 6-16

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,1 +1,26 @@
 // The second IIFE should augment the original one with a function that creates all of the eventHandlers that you need for the application. Name the function activateEvents.
+
+  // CarLot.activateEvents();
+
+var CarLot = (function (carlot) {
+
+  carlot.activateEvents = function(){
+  	var cars = CarLot.getInventory();
+
+
+  	for (i = 0; i < cars.length; i++){
+	  	var carCard = document.getElementById(`vehicle--${i}`);
+	  	console.log("carCard", carCard);
+	  	carCard.addEventListener("click", editCard);
+
+  		
+  	};
+
+
+
+  	// submitBtn.addEventListener("click", changeDescription);
+  };
+
+  return carlot;
+
+})(CarLot || {});
--- quiz.js
+++ quiz.js
@@ -1,11 +1,17 @@
+var textInputEl = document.getElementById("editInput");
+var submitBtn = document.getElementById("submitBtn");
+
+
+
 function populatePage (inventory) {
 	// console.log("inventory", inventory);
   // Loop over the inventory and populate the page
   // Loop over your array of cars and build up an HTML string to build a card for each car. Also, use Bootstrap to create rows. Each row should contain 3 columns. Make sure you have a parent element with a class of container.
   // Make sure you display all properties of the car in the DOM. Basic styling is up to you.
+  var carRow = document.getElementById("carRow");
   var outputEl = document.getElementById("carContainer");
   var carTotal = inventory.length;
-  console.log("number of cars on lot:", carTotal);
+
   columnCounter = 1; //counter for liniting 3 columns per row
 
   // for (i = 0; i < carTotal; i++){
@@ -41,18 +47,19 @@ function populatePage (inventory) {
 	  		newCarPriceEl.innerHTML = `$${carPrice}`;
 	  		newCarPriceEl.classList.add("carPrice");
 	  		newCarCard.appendChild(newCarPriceEl);
+
 	  		var newCarDescriptEl = document.createElement("p");
 	  		newCarDescriptEl.innerHTML = carDescript;
 	  		newCarDescriptEl.classList.add("carDescript");
+	  		var descriptionID = document.createAttribute("id");
+	  		descriptionID.value = `description--${indexNum}`;
+	  		newCarDescriptEl.setAttributeNode(descriptionID);
 	  		newCarCard.appendChild(newCarDescriptEl);
 
+	  		newCarCard.classList.add("col-sm-4");
+	  		newCarCard.classList.add("col-xs-12");
 
-
-	  		outputEl.appendChild(newCarCard);
-
-
-
-
+	  		carRow.appendChild(newCarCard);
 
 	  		// columnCounter += 1;
 	  	});
@@ -72,9 +79,52 @@ function populatePage (inventory) {
 
   // Now that the DOM is loaded, establish all the event listeners needed
 
-  // CarLot.activateEvents();
+  CarLot.activateEvents();
 }
 
+function editCard (event) {
+	var selectedEl = event.target;
+	selectedEl.classList.toggle("selectedBorder");
+	var cardSelectedNum = event.target.id.split("--")[1];
+	console.log("you clicked on card #", cardSelectedNum);
+	textInputEl.focus();
+	textInputEl.value = "";
+}
+
+
+
+// function changeDescription () (carIDnum) {
+
+// }
+
+
+// function changeText() {
+// 	// 8. When you click on one of the person elements, a dotted border should appear around it.
+// 	var selectedEl = event.target;
+// 	selectedEl.classList.toggle("selectedBorder");
+
+// // 9. When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
+// 	textInput.focus();
+// 	textInput.value = "";
+// // 10. When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
+// 	liveUpdate(selectedEl);
+// }
+
+
+// function liveUpdate(element) {
+// 	textInput.addEventListener('keyup', function(){
+// 			element.innerHTML = textInput.value;
+// 	});
+// // 11. When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
+// 	textInput.addEventListener('keypress', function (e) {
+//     var key = e.keyCode;
+//     if (key === 13) { // 13 is enter
+//     		element.classList.toggle("selectedBorder");
+//     		textInput.value = "";
+//     		element = "";
+//     };
+// 	});
+// }
 
 
 
